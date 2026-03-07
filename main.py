e7b036bf59f414be013657ab4615b61bc5dd2f9b
Zach Massarella <88383060+ZachMassarella@users.noreply.github.com>
2026-03-07 16:26:17 +0100

    Initialisation of Potential Framework

---
+++ main.py
@@ -0,0 +1,17 @@
+class NeuralNetwork:
+    def __init__(self, activation_function, learning_rate, num_hidden_layers):
+        self.activation_function: str = activation_function
+        self.learning_rate: float = learning_rate
+        self.num_hidden_layers: int = num_hidden_layers
+    
+    def cost():
+        return
+    
+    def relu():
+        return
+    
+    def feed_forward():
+        return
+    
+    def run_nn():
+        return
